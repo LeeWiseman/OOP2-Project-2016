b7337b514e4041bd0b0c3c92f85dcf03a79136ae
Lee Wiseman <t00187940@365s.ittralee.ie>
2016-12-01 14:02:12 +0000

    JText

--- ChoicePage.java
+++ ChoicePage.java
@@ -5,11 +5,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ChoicePage extends JFrame{
+	
+	JButton randomNumGame;
+	JButton chooseNumGame;
+	
 	public ChoicePage(){
 
 	setSize(600,800);
 	setTitle("Welcome!");
-	setVisible(true);	
+	setVisible(true);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);	
 	//Set window logo
 	ImageIcon logoicon = new ImageIcon("MoneyBag.png");
 	Image logo = logoicon.getImage();
@@ -17,8 +22,21 @@ public class ChoicePage extends JFrame{
 	//position
 	setResizable(false);
 	setLocation(700,100);
-	JButton randomNumGame = new JButton("Random Number");
-	JButton chooseNumgame = new JButton("Choose Your Number");
+	
+	Container pane1 = getContentPane();
+	pane1.setLayout(null);
+	randomNumGame = new JButton("Random Number");
+	chooseNumGame = new JButton("Choose Your Number");
+	
+		pane1.add(randomNumGame);
+			randomNumGame.setBounds(90,500,200,40);
+		pane1.add(chooseNumGame);
+			chooseNumGame.setBounds(310,500,200,40);
+			JOptionPane.showInputDialog("Enter the amount you want to bet");
+	/*JTextField bet = new JTextField("Your Bet Amount");
+	pane1.add(bet);
+	bet.setBounds(290,400,200,40);
+	*/
 		}
 	
 	
